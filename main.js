62d8ee27a5c9c3184be43f00d5af875d7c6fb2d5
Nadja Brandys <nadiusza@gmail.com>
2020-06-28 22:40:55 +0200

    Remove frame and dev tools

--- main.js
+++ main.js
@@ -10,14 +10,15 @@ function main() {
     webPreferences: {
       nodeIntegration: true
     },
-    width: 800,
-    height: 600,
+    width: 650,
+    height: 520,
+    frame: false,
   })
 
   // load app/index.html as the window content
   mainWindow.loadFile(path.join('app', 'index.html'));
 
-  mainWindow.webContents.openDevTools();
+
 }
 
 app.on('ready', main);
